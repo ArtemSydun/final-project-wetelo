455a54d359e7e92a1789ce9d4f0ca14ce0e0bfaf
Artem Sydun <sydun2003@gmail.com>
2024-06-08 22:40:32 +0300

    redundant comments deleted from app.module file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,8 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
-// import { SequelizeModule } from '@nestjs/sequelize';
-// import { ConfigModule } from '@nestjs/config';
-// import User from './database/models/users';
 import { UsersModule } from './modules/users/user.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { MailModule } from './modules/mailer/mail.module';
 import { PostsModule } from './modules/posts/posts.module';
-// import PostModel from './database/models/posts';
 import { SequelizeConnectionModule } from './database/config/sequelize.module';
 import { LoggerMiddleware } from './middlewares/logger.middleware';
 
